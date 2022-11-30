e08d7f3a5483742ae9f8b2b1e91c384a3d1876f0
Alex Li <alexliinc@gmail.com>
2022-11-30 15:09:25 -0800

    LFG!

---
+++ pages/api/generate.js
@@ -0,0 +1,29 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = `
+Write me a fan fiction story in the style of R.J. Anderson with the title below.
+
+Title:
+`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API CALL: ${basePromptPrefix}${req.body.userInput}\n`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
